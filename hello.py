2ab9b2ac16492f83bd5adace475cd71f6442e0aa
Kevin <neoglitch822@yahoo.com>
2023-12-28 15:00:41 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 # David Malan's CS50P class. Lecture 0 - Functions and Variables
 name = input("What's your name? ")
-print("hello, " + name)
+name = name.strip()
+name = name.title()
+print(f"hello, {name}")
