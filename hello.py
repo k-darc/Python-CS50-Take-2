2b08bd68f62a4ee5fb076737e463fe578cb7984c
Kevin <neoglitch822@yahoo.com>
2023-12-28 15:02:15 -0500

    Update hello.py
    Capitalizes first letter of name

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # David Malan's CS50P class. Lecture 0 - Functions and Variables
-name = input("What's your name? ")
-name = name.strip()
-name = name.title()
+
+name = input("What's your name? ").strip().title()
+
 print(f"hello, {name}")
