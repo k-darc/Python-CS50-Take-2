313dc9b71e3279055a2ad2ee21bcf63c40a82299
Kevin <neoglitch822@yahoo.com>
2023-12-28 16:30:05 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # David Malan's CS50P class. Lecture 0 - Functions and Variables
-def hello(to):
+def hello(to="world"):
     print("hello,", to)
 
-
+hello()
 name = input("What's your name? ")
 hello(name)
