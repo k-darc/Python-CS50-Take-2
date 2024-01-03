75a8cec90354a99f8bb3195055e95e29b77f1de5
Kevin <neoglitch822@yahoo.com>
2024-01-03 14:58:41 -0500

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,7 +1,7 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x != y:
-    print("x is NOT equal to y")
-else:
+if x == y:
     print("x is EQUAL to y")
+else:
+    print("x is NOT equal to y")
