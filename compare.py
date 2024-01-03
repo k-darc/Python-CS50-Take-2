b59bfbf7afa983c946a6ae6772817e7d128cd876
Kevin <neoglitch822@yahoo.com>
2024-01-03 14:40:09 -0500

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("X is less than y")
+if x > y:
+    print("X is greather than y")
+if x == y:
+    print("X is equal to y")
