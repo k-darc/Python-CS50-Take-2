ea462b2e42783e1ac5cefd55e4fb1abc77034d6d
Kevin <neoglitch822@yahoo.com>
2024-01-08 11:33:48 -0500

    Update number.py

--- number.py
+++ number.py
@@ -1,2 +1,6 @@
-x = int(input("What's x? "))
-print(f"x is {x}")
+# Lecture 3 - Exceptions
+try:
+    x = int(input("What's x? "))
+    print(f"x is {x}")
+except ValueError:
+    print("x is not an integer")
