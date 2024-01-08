90d2585efcaee8b91a8f10930da8ce978325679f
Kevin <neoglitch822@yahoo.com>
2024-01-08 10:25:48 -0500

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,5 @@
-students = ["Hermione", "Harry", "Ron"]
-
-for i in range(len(students)):
-    print(i +, students[i])
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor"
+    }
