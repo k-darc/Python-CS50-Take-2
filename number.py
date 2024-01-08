fd86833c7805e2057a5916b26157c5a809ac4888
Kevin <neoglitch822@yahoo.com>
2024-01-08 11:55:08 -0500

    Update number.py

--- number.py
+++ number.py
@@ -1,10 +1,16 @@
 # Lecture 3 - Exceptions
-while True:
-    try:
-        x = int(input("What's x? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
+def main():
+    x = get_int()
+    print(f"x is {x}")
 
-print(f"x is {x}")
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    return x
+
+main()
