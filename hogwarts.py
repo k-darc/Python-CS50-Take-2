d97499109baacb288092e46340323a7334455226
Kevin <neoglitch822@yahoo.com>
2024-01-08 10:44:59 -0500

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -1,9 +1,9 @@
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin"
-    }
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"])
