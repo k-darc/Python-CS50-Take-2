9294071973c33ef163976cf89634e905814b4916
Kevin <neoglitch822@yahoo.com>
2024-01-09 15:10:28 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 # David Malan's CS50P class. Lecture 0 - Functions and Variables
 def main():
     name = input("What's your name? ")
-    hello(name)
+    print(hello(name))
 
 def hello(to="world"):
-    print("hello,", to)
+    return f"hello, {to}"
 
 if __name__ == "__main__":
     main()
