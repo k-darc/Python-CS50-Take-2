0b6d01244d0cbff864316eda8a109ba3da2573be
Kevin <neoglitch822@yahoo.com>
2024-01-10 13:31:00 -0500

    Update names.py

--- names.py
+++ names.py
@@ -1,6 +1,6 @@
 # Lecture 6 - File i/o
 name = input("What's your name? ")
 
-file = open("names.txt", "a")
-file.write(f"{name}\n")
-file.close()
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
