c895e67c9ea226c93e6e7af6031aa7c2dd470d65
Kevin <neoglitch822@yahoo.com>
2024-01-10 13:21:17 -0500

    Update names.py

--- names.py
+++ names.py
@@ -1,2 +1,8 @@
-name = input("What's your name? ")
-print(f"hello, {name}")
+# Lecture 6 - File i/o
+names = []
+
+for _ in range(3):
+    names.append(input("What's your name? "))
+
+for name in sorted(names):
+    print(f"hello, {name}")
