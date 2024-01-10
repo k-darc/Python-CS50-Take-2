f34e5008d6b2b8134098f911fba70a2b7f75fd9a
Kevin <neoglitch822@yahoo.com>
2024-01-10 13:36:48 -0500

    Update names.py

--- names.py
+++ names.py
@@ -1,6 +1,6 @@
 # Lecture 6 - File i/o
-name = input("What's your name? ")
+with open("names.txt", "r") as file:
+    lines = file.readlines()
 
-
-with open("names.txt", "a") as file:
-    file.write(f"{name}\n")
+for line in lines:
+    print("hello,", line.rstrip())
