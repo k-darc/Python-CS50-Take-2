6094c3687e0406c7277e2797d9c07c9e7a895901
Kevin <neoglitch822@yahoo.com>
2024-01-10 15:30:34 -0500

    Update students.py

--- students.py
+++ students.py
@@ -4,5 +4,5 @@ name = input("What's your name? ")
 home = input("Where's your home? ")
 
 with open("students.csv", "a") as file:
-    writer = csv.writer(file)
+    writer = csv.DictWriter(file)
     writer.writerow([name, home])
