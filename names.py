93e4295d3a63ffa5a90965178133ad7ff3c7951a
Kevin <neoglitch822@yahoo.com>
2024-01-10 13:27:13 -0500

    Update names.py

--- names.py
+++ names.py
@@ -1,8 +1,6 @@
 # Lecture 6 - File i/o
-names = []
+name = input("What's your name? ")
 
-for _ in range(3):
-    names.append(input("What's your name? "))
-
-for name in sorted(names):
-    print(f"hello, {name}")
+file = open("names.txt", "a")
+file.write(name)
+file.close()
