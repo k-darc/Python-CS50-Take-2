9cf6d158ea9438538f6d1bea59edb4c158b56699
Kevin <neoglitch822@yahoo.com>
2024-01-10 13:37:15 -0500

    Update names.py

--- names.py
+++ names.py
@@ -1,6 +1,4 @@
 # Lecture 6 - File i/o
 with open("names.txt", "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    print("hello,", line.rstrip())
+    for line in file:
+        print("hello,", line.rstrip())
