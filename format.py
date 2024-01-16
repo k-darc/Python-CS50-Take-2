973006547ce643410bc6c642dee029fc3193b289
Kevin <neoglitch822@yahoo.com>
2024-01-16 17:46:58 -0500

    Update format.py

--- format.py
+++ format.py
@@ -3,6 +3,5 @@ import re
 name = input("What's your name? ").strip()
 matches = re.search(r"^(.+), (.+)$", name)
 if matches:
-    last, first = matches.group()
-    name = f"{first} {last}"
+    name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
