72f0f8f941a97ada1d8ff1a1a4998eda2b37c439
Kevin <neoglitch822@yahoo.com>
2024-01-17 10:53:34 -0500

    Update student.py

--- student.py
+++ student.py
@@ -7,9 +7,9 @@ def main():
     print(f"{student.name} from {student.house}")
 
 def get_student():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
+    name = input("Name: ")
+    house = input("House: ")
+    Student(name, house)
     return student
 
 if __name__ == "__main__":
