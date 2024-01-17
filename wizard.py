1437d43eaee13320eeeb61ae470491fe60da3608
Kevin <neoglitch822@yahoo.com>
2024-01-17 14:35:50 -0500

    Update wizard.py

--- wizard.py
+++ wizard.py
@@ -1,16 +1,27 @@
-class Student:
-    def __init__(self, name, house):
-        of not name:
-            raise ValueError:("Missing name")
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError("Missing name")
         self.name = name
+
+    ...
+
+
+class Student(Wizard):
+    def __init__(self, name, house):
+        super().__init__(name)
         self.house = house
 
     ...
 
 
-class Professor:
+class Professor(Wizard):
     def __init__(self, name, subject):
-        self.name = name
+        super().__init__(name)
         self.subject = subject
 
     ...
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against the Dark Arts")
