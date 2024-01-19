e9977a2f51fda9dcfa42afc098a67611c258c340
Kevin <neoglitch822@yahoo.com>
2024-01-19 11:10:02 -0500

    Update houses.py

--- houses.py
+++ houses.py
@@ -5,3 +5,11 @@ students = [
     {"name": "Draco", "house": "Slytherin"},
     {"name": "Padma", "house": "Ravenclaw"},
 ]
+
+houses = []
+for student in students:
+    if student["house"] not in houses:
+        houses.append(student["house"])
+
+for house in sorted(houses):
+    print(house)
