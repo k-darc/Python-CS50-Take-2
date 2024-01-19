70e68c298ce874ea57a554cd1e03dd85aa5f0acd
Kevin <neoglitch822@yahoo.com>
2024-01-19 11:40:57 -0500

    Update meows.py
    type ends

--- meows.py
+++ meows.py
@@ -1,10 +1,3 @@
-class Cat:
-    MEOWS = 3
-
-    def meow(self):
-        for _ in range(Cat.MEOWS):
-            print("meow")
-
-
-cat = Cat()
-cat.meow()
+def meow(n):
+    for _ in range(n):
+        print("meow")
