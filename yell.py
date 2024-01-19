fe394badf0cc0ad901ec36fe435ad3572eb021ef
Kevin <neoglitch822@yahoo.com>
2024-01-19 14:42:40 -0500

    Update yell.py

--- yell.py
+++ yell.py
@@ -6,7 +6,7 @@ def yell(words):
     uppercased = []
     for word in words:
         uppercased.append(word.upper())
-    print(uppercased)
+    print(*uppercased)
 
 if __name__ == "__main__":
     main()
