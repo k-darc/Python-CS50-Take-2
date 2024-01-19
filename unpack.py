ebca70d92ba90f3faeef4fd377b6bfc633d3824c
Kevin <neoglitch822@yahoo.com>
2024-01-19 12:50:11 -0500

    Update unpack.py

--- unpack.py
+++ unpack.py
@@ -1,8 +1,5 @@
-def total(galleons, sickles, knuts):
-    return (galleons * 17 + sickles) * 29 + knuts
+def f(*args, **kwargs):
+    print("Positional:", args)
 
 
-coins = {"galleons": 100, "sickles": 50, "knuts": 25}
-
-
-print(total(**coins), "Knuts")
+f(100, 50, 25)
