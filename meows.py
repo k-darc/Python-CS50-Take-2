76a3e347105225b4926648f53a3d598085729c4f
Kevin <neoglitch822@yahoo.com>
2024-01-19 12:08:45 -0500

    Update meows.py

--- meows.py
+++ meows.py
@@ -1,16 +1,6 @@
-def meow(n: int) -> str:
-    """
-    Meow n times.
+import sys
 
-    :param n: Number of times to meow
-    :type n: int
-    :raise TypeError: If n is not an int
-    :return: A string of n meows, one per line
-    :rtype: str
-    """
-    return "meow\n" * n
-
-
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end="")
+if len(sys.argv) == 1:
+    print("meow")
+else:
+    print("usage: meows.py")
