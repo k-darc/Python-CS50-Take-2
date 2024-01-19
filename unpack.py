aff47f4bae06d54feebcfecdc55a866a84cf0b0e
Kevin <neoglitch822@yahoo.com>
2024-01-19 12:26:51 -0500

    Update unpack.py

--- unpack.py
+++ unpack.py
@@ -1,2 +1,4 @@
-first, _ = input("What's your name? ").split(" ")
-print(f"hello, {first}")
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
+
+print(total(100, 50, 25), "Knuts")
