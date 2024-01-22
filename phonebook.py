823f4f74f479ec8192ca7e70bf88b892b13b66ce
Kevin <neoglitch822@yahoo.com>
2024-01-22 17:12:02 -0500

    Update phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -1,8 +1,16 @@
-names = ["Carter", "David", "John"]
+people = [
+    {"name": "Carter", "number": "+1-732-555-5555"},
+    {"name": "David", "number": "+1-752-555-5555"},
+    {"name": "John", "number": "+1-908-555-5555"},
+]
 
 name = input("Name: ")
 
-if name in names:
-    print("Found")
+for person in people:
+    if person["name"] == name:
+        number = person["number"]
+        print(f"Found {number}")
+        break
+
 else:
     print("Not found")
