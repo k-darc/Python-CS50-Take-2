5b627ce1d2471a44d85b2255572c743f4414a11a
Kevin <neoglitch822@yahoo.com>
2024-01-22 16:49:14 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,24 @@
+# def main():
+#     x = int(input("What's x? "))
+#     print("x squared is", square(x))
+
+# def square(n):
+#     return n * n
+
+# if __name__ == "__main__":
+#     main()
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Not an integer")
+
 def main():
-    x = int(input("What's x? "))
-    print("x squared is", square(x))
+    x = get_int("x: ")
+    y = get_int("y: ")
 
-def square(n):
-    return n * n
+    print( x + y)
 
-if __name__ == "__main__":
-    main()
+main()
